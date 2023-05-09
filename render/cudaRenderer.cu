3942386c5c0e3cdedd6f2e51362d5accf5c4e464
lr6868 <97738341+lr6868@users.noreply.github.com>
2023-05-09 08:57:24 +0800

    deduce duplicate cacuylation for pixelCenterNorm

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -525,6 +525,9 @@ __global__ void kernelRenderCircles() {
     float4* imgPtr = (float4*)(&cuConstRendererParams.imageData[4 * (py * imageWidth + px)]);
     float4 color;
     color = *imgPtr;
+    
+    float2 pixelCenterNorm = make_float2(invWidth * (static_cast<float>(px) + 0.5f),
+                                        invHeight * (static_cast<float>(py) + 0.5f));
     for (int start=0; start< cuConstRendererParams.numCircles;start+=BLOCKSIZE){//是否要将cuConstRendererParams.numCircles变成const？
         
         int index=start+tIdx; 
@@ -553,8 +556,6 @@ __global__ void kernelRenderCircles() {
         for(short i=0;i<numDefiniteCircles;i++){
                 k=inSection[i];
                 float3 p1 = *(float3*)(&cuConstRendererParams.position[3 * k]);
-                float2 pixelCenterNorm = make_float2(invWidth * (static_cast<float>(px) + 0.5f),
-                                                    invHeight * (static_cast<float>(py) + 0.5f));
                 shadePixel(k, pixelCenterNorm, p1, &color);
             }
         __syncthreads();
